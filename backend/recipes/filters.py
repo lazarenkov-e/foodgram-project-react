70a52281b1eb84733d771d1762748566815032ef
Евгений Лазаренков <lazarenkov88@gmail.com>
2023-06-11 15:56:49 +0300

    Deploy

--- backend/recipes/filters.py
+++ backend/recipes/filters.py
@@ -26,7 +26,7 @@ class RecipeFilter(FilterSet):
 
     def filter_is_favorited(self, queryset, name, value):
         if self.request.user.is_authenticated and value:
-            return queryset.filter(favorites__user=self.request.user)
+            return queryset.filter(favorites_recipe__user=self.request.user)
         return queryset
 
     def filter_is_in_shopping_cart(self, queryset, name, value):
